7c4a4710fedfb174fae40a7c18a0396b049df8fe
Edward Wang <edwardwang@Edwards-MBP-4.wv.cc.cmu.edu>
2016-11-03 12:50:18 -0400

    Iphone pinch2

--- site/app.coffee
+++ site/app.coffee
@@ -226,8 +226,8 @@ window.App = class App
     [x0,x1,x2,x3,y0,y1,y2,y3] = [-1,-1,-1,-1,-1,-1,-1,-1]
     isPinching = false
     pinchZoom = () =>
-      d1 = Math.sqrt((x0-x1)^2+(y0-y1)^2)
-      d2 = Math.sqrt((x2-x3)^2+(y2-y3)^2)
+      d1 = ((x0-x1)^2+(y0-y1)^2)
+      d2 = ((x2-x3)^2+(y2-y3)^2)
       alert(x0+"|"+x1+"|"+x2+"|"+x3+"|"+y0+"|"+y1+"|"+y2+"|"+y3)
       alert(d1+"|"+d2)
       if d1 > d2
--- site/app.js
+++ site/app.js
@@ -446,8 +446,8 @@ window.App = App = (function() {
     pinchZoom = (function(_this) {
       return function() {
         var d1, d2;
-        d1 = Math.sqrt((x0 - x1) ^ 2 + (y0 - y1) ^ 2);
-        d2 = Math.sqrt((x2 - x3) ^ 2 + (y2 - y3) ^ 2);
+        d1 = (x0 - x1) ^ 2 + (y0 - y1) ^ 2;
+        d2 = (x2 - x3) ^ 2 + (y2 - y3) ^ 2;
         alert(x0 + "|" + x1 + "|" + x2 + "|" + x3 + "|" + y0 + "|" + y1 + "|" + y2 + "|" + y3);
         alert(d1 + "|" + d2);
         if (d1 > d2) {
