248747f3a9b008c3085030e11cde9d780c114f3f
Samuel Gratzl <sam@sgratzl.com>
2020-12-08 10:47:44 -0500

    build: add docker setup

--- build.js
+++ build.js
@@ -1,12 +1,12 @@
-const { process, minimize, compile, replace } = require('./devops/buildTools');
+const { process, minimizeJs, compileCoffee, replace } = require('delphi-cmu-buildtools');
 
 const strings = require('./values/strings.json');
 const colors = require('./values/colors.json');
 const entries = [...strings, ...colors];
 
 Promise.all([
-    process('**/*.coffee', [compile(), minimize()]),
-    process('**/*.js', [minimize()]),
+    process('**/*.coffee', [compileCoffee(), minimizeJs()]),
+    process('**/*.js', [minimizeJs()]),
     process('**/*.+(html|css)', [replace(entries)]),
     process('+(plots|images)/**/*')
 ]).then((r) => console.log(r.flat()));
